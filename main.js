6433f9589cc7ccdd2eff53270495cd48c74c0c18
AmitNirGit <67857460+AmitNirGit@users.noreply.github.com>
2020-08-03 14:47:29 +0300

    task 1 complete

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const searchPokemon = async (pokemonId = 3) => {
-  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
-};
-searchPokemon();
+const searchInput = document.getElementById("search");
+const searchButton = document.getElementById("searchButon");
 
+const searchPokemon = async (pokemonId) => {
+  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+  makeDiv(data.name, data.height, data.weight, data.sprites.front_default);
+};
+
+const makeDiv = (name, height, weight, picture) => {
+  const htmlText = `
+      <div class="pokemonContainer">
+        <div>Name: ${name}</div>
+        <div>height: ${height}</div>
+        <div>weight: ${weight}</div>
+        <div>picture: <img src="${picture}" /></div>
+      </div>
+    `;
+  pokemonDiv.innerHTML = htmlText;
+}
+
+
+
+searchButton.addEventListener("click", () => {
+  searchPokemon(searchInput.value);
+})
